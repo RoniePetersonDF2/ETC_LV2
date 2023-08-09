9e60cde09a7bede5d0ad5d90f15f7c295afe688d
RoniePetersonDF2 <126265898+RoniePetersonDF2@users.noreply.github.com>
2023-08-09 12:37:36 -0300

    Update resultado.php

--- 02/exercicio01/resultado.php
+++ 02/exercicio01/resultado.php
@@ -1,5 +1,17 @@
 <?php
-
-
+    header('Content-Type: text/html; charset=UTF-8');
+    
+    if (!isset($_POST['email']) or empty($_POST['email'])) 
+    {
+        echo "<p>O campo e-mail está vazio ou não foi enviado.</p>";
+    } elseif (!isset($_POST['password']) or empty($_POST['password'])) 
+    {
+        echo "<p>O campo password está vazio ou não foi enviado.</p>";
+    } else {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        echo "<p>O e-mail enviado foi: <b>$email</b></p>";
+        echo "<p>A senha enviada foi: <b>$password</b></p>";
+    }
 ?>
 <a href="index.php">Voltar</a>

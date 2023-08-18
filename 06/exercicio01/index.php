72828f3388d450576b26d6e15ef3a8e39f130cee
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-08-18 17:18:20 -0300

    Add aula06/exercicio01

--- 06/exercicio01/index.php
+++ 06/exercicio01/index.php
@@ -7,22 +7,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Banco ETC</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <h1>Banco ETC</h1>
+    <main class="principal">
+        <h1>Banco ETC</h1>
 
-    <h2>Abrir Conta</h2>
-    <form method="get">
-        <label for="">Número</label><br>
-        <input type="text" name="numero" placeholder="Informe o número da conta." required autofocus><br>
-        <label for="">Cliente</label><br>
-        <input type="text" name="cliente" placeholder="Informe o nome do cliente da conta." size="30" required><br>
-        <label for="">Saldo</label><br>
-        <input type="text" name="numero" placeholder="Informe o saldo inicial." required><br><br>
-        <input type="submit" value="Salvar" name="botaoSalvar">
-    </form>
+        <h2>Escolha uma das opções</h2>
 
-    <hr>
-    <h2>Informações sobre a conta</h2>
+        <section class="secao__opcao__botao">
+            <a href="abrirconta.php">Abrir Conta</a>
+            <a href="#">Verificar Saldo</a>
+            <a href="#">Sacar</a>
+            <a href="#">Depositar</a>
+        </section>
+    </main>
 </body>
 </html>

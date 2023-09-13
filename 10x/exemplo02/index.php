904591a74997ec8dd186bbba3579cd6e26e1c0fe
RoniePetersonDF2 <126265898+RoniePetersonDF2@users.noreply.github.com>
2023-09-13 09:47:57 -0300

    Add files via upload

--- 10x/exemplo02/index.php
+++ 10x/exemplo02/index.php
@@ -1,11 +1,24 @@
-<?php
-    include 'src/conexao.php';
-
-    # chamando um método estático sem atributos
-    echo "<p>Chamando meu método estático: " . Conexao::minhaString() . "</p>";
-
-    # chamando um método estático que usa um atributo estatico
-    echo "<p>Meu endereço de servidor: " . Conexao::$servidor . "</p>";
-    
-    # chamando um método estático que usa um atributo estatico privado
-    echo "<p>Minha string de conexao: " . Conexao::minhaConexao() . "</p>";
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form>
+        <label for="">Valor 1</label><br>
+        <input type="number" name="valor1" placeholder="Informe o valor 1" required autofocus> <br>
+        <label for="">Valor 2</label><br>
+        <input type="number" name="valor2" placeholder="Informe o valor 2" required> <br>
+        <input type="radio" name="operacao" value="somar" checked> Somar (+) 
+        <input type="radio" name="operacao" value="subtrair"> Subtrair (-) 
+        <input type="radio" name="operacao" value="multiplicar"> Multiplicar (x) 
+        <br>
+        <input type="submit" value="Executar" name="executar">
+    </form>
+    <hr>
+    <h2>Resultado</h2>
+    <?php include 'src/operacao.php';?>
+</body>
+</html>

ca02c63ffe0a2dc2b945e9df925e59f6f35f190c
RoniePetersonDF2 <126265898+RoniePetersonDF2@users.noreply.github.com>
2023-10-06 08:40:47 -0300

    Add aula15 exercicio02

---
+++ 15/exercicio02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01 - Home</title>
+</head>
+<body>
+    <h1>Home</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="usuarios/index.php">Usuários</a></li>
+            <li><a href="#">Perfil</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
